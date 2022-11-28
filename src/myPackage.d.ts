a93974e950183cfeac74acc3221aceccd504b455
servantcdh <servantcdh@gmail.com>
2022-11-28 19:06:29 +0900

    UPDATE: practice typescript by blockchain sample

--- src/myPackage.d.ts
+++
@@ -1,9 +0,0 @@
-interface Config {
-    url: string;
-}
-
-declare module "myPackage" {
-    /** 이렇게 함수 설명을 적으면 됩니다 */
-    function init(config: Config): boolean;
-    function exit(code: number): boolean;
-}
