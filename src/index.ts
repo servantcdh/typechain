81a43ef549c8ce082e1a059726ad228f4a4d09ca
servantcdh <servantcdh@gmail.com>
2022-11-28 16:51:24 +0900

    UPDATE: update tsconfig (allowJs)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { init, exit } from "myPackage";
 
 init({
-    url: ""
+    url: ''
 });
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,19 @@
+//  @ts-check
+/**
+ * 프로젝트를 초기화합니다.
+ * @param {object} config 
+ * @param {string} config.url
+ * @returns {boolean}
+ */
 export function init(config) {
     return true;
 };
 
+/**
+ * exit the program
+ * @param {number} code 
+ * @returns {number}
+ */
 export function exit(code) {
     return code + 1;
 }
